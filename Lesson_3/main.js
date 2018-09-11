1545270a0440383a5d59db5cb220cf0d1c2dd89b
Александр Крайнов <kraleksande@gmail.com>
2018-09-12 00:05:52 +0300

    L3v3

--- Lesson_3/main.js
+++ Lesson_3/main.js
@@ -1,7 +1,35 @@
 // === 1 ===
 console.log('=== 1 ===')
 
+let a1 = '';
+let p, n = 0;
+while (n <= 100) {
+	if (n == 2 || n == 3 || n == 5 || n == 7) {
+		//console.log(n)
+		a1 += n + ' ';
+	} else if (n % 2 !== 0 && n % 3 !== 0 && n % 5 !== 0 && n % 7 !== 0) {
+		//console.log(n);
+		a1 += n + ' ';
+	}
+	n++;
+}
+console.log(a1);
 
+let a2 = '';
+let v = 0;
+while (v <= 100) {
+	for (var g = 2; g <= v; g++) {
+		if (v % g == 0) {
+			break;
+		}
+	}
+	if (v == g) {
+		//console.log(v);
+		a2 += v + ' ';
+	}
+	v++;
+}
+console.log(a2);
 
 
 // === 2 ===
