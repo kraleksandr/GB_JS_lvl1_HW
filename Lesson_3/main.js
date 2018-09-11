6d2efebb1eb4150875e240f87e89353cf605f1b7
Александр Крайнов <kraleksande@gmail.com>
2018-09-11 22:53:28 +0300

    L3v1

---
+++ Lesson_3/main.js
@@ -0,0 +1,26 @@
+// === 1 ===
+console.log('=== 1 ===')
+
+
+
+
+// === 2 ===
+console.log('=== 2 ===')
+
+
+
+
+// === 3 ===
+console.log('=== 3 ===')
+
+for (let d = 0; d <= 9; console.log(d++)) {}
+
+
+// === 4 ===
+console.log('=== 4 ===')
+
+let c = '';
+do {
+	c += 'x';
+	console.log(c);
+} while (c.length < 20)
