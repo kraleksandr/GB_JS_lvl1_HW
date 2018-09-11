2639692cec084e4723091a4df561d41511e9b638
Александр Крайнов <kraleksande@gmail.com>
2018-09-11 23:04:10 +0300

    L3v2

--- Lesson_3/main.js
+++ Lesson_3/main.js
@@ -7,7 +7,19 @@ console.log('=== 1 ===')
 // === 2 ===
 console.log('=== 2 ===')
 
-
+let a = 0;
+do {
+	let aa = a;
+	if (a == 0) {
+		aa += ' Эт ноль'
+	} else if (a % 2 == 0) {
+		aa += ' Чет'
+	} else {
+		aa += ' Нечет'
+	}
+	console.log(aa);
+	a++;
+} while (a <= 10)
 
 
 // === 3 ===
